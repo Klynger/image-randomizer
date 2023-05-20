2c7aac971abdf7ab9cf209723a6f79b62ee394c3
Rafael Klynger <rafael.klynger@gmail.com>
2023-05-20 18:39:56 -0300

    Fix prettier

--- src/modules/fetchFromkv.ts
+++ src/modules/fetchFromkv.ts
@@ -1,17 +1,17 @@
-import { getKvEndpoint } from "./getKvEndpoint";
+import { getKvEndpoint } from './getKvEndpoint'
 
 export function fetchFromKv(key: string) {
   const endpoint = getKvEndpoint(
     process.env.CLOUDFLARE_ACCOUNT_ID as string,
     process.env.CLOUDFLARE_KV_NAMESPACE_ID as string,
-    key
-  );
+    key,
+  )
 
   return fetch(endpoint, {
-    method: "GET",
+    method: 'GET',
     headers: {
       Authorization: `Bearer ${process.env.CLOUDFLARE_KV_API_TOKEN}`,
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
-  }).then((res) => res.text());
+  }).then((res) => res.text())
 }
--- src/modules/getKvEndpoint.ts
+++ src/modules/getKvEndpoint.ts
@@ -1,3 +1,7 @@
-export function getKvEndpoint(accountId: string, namespaceId: string, key: string,) {
-  return `https://api.cloudflare.com/client/v4/accounts/${accountId}/storage/kv/namespaces/${namespaceId}/values/${key}`;
+export function getKvEndpoint(
+  accountId: string,
+  namespaceId: string,
+  key: string,
+) {
+  return `https://api.cloudflare.com/client/v4/accounts/${accountId}/storage/kv/namespaces/${namespaceId}/values/${key}`
 }
