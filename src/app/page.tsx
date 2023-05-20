2129766111184d73adf3b1f40a5d2122053af4c1
Rafael Klynger <rafael.klynger@gmail.com>
2023-05-20 19:11:32 -0300

    Add content again

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,34 @@
-// import { fetchFromKv } from '@/modules/fetchFromkv'
+import { fetchFromKv } from '@/modules/fetchFromkv'
 
-// const IMAGE_COUNT_KEY = 'image-count'
+const IMAGE_COUNT_KEY = 'image-count'
 
 async function Home() {
-  // const imageCount = await fetchFromKv(IMAGE_COUNT_KEY)
+  const imageCount = await fetchFromKv(IMAGE_COUNT_KEY)
 
-  // const imageUrl = await getImageUrl(imageCount)
+  const imageUrl = await getImageUrl(imageCount)
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Image count: {0}
+      Image count: {imageCount}
       <div className="h-80 w-80 border border-black">
-        {/* <img src={imageUrl} alt="Something" /> */}
+        <img src={imageUrl} alt="Something" />
       </div>
     </main>
   )
 }
 
-// async function getImageUrl(imageCountStr: string) {
-//   const imageCount = parseInt(imageCountStr, 10)
+async function getImageUrl(imageCountStr: string) {
+  const imageCount = parseInt(imageCountStr, 10)
 
-//   const imageIndex = imageCount - 1
+  const imageIndex = imageCount - 1
 
-//   const imageId = await fetchFromKv(getImageKey(imageIndex))
+  const imageId = await fetchFromKv(getImageKey(imageIndex))
 
-//   return `https://images.prismic.io/getcircuit/${imageId}`
-// }
+  return `https://images.prismic.io/getcircuit/${imageId}`
+}
 
-// function getImageKey(imageIndex: number) {
-//   return `image-${imageIndex}`
-// }
+function getImageKey(imageIndex: number) {
+  return `image-${imageIndex}`
+}
 
 export default Home
